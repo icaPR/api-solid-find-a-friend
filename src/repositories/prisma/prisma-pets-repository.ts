50815d7319c67f2b3a5340ad3f72a245df9eb182
Icaro Ramos <polosnramos@hotmail.com>
2024-05-24 22:54:04 -0300

    feat: Create PrismaPetsRepository for pet creation

---
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -0,0 +1,10 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+import { PetsRepository } from "../pets-repository";
+
+export class PrismaPetsRepository implements PetsRepository {
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({ data });
+    return pet;
+  }
+}
