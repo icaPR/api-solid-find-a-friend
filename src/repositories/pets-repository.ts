b280b2706ed7ee0e6587daf642bea1a75c57d0b3
Icaro Ramos <polosnramos@hotmail.com>
2024-05-26 23:29:04 -0300

    feat: Add fetchByAttributes function to PetsRepository interface

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,6 +1,20 @@
 import { Pet, Prisma } from "@prisma/client";
 
+interface FilterProps {
+  age?: "PUPPY" | "ADULT" | "ELDERLY";
+  size?: "SMALL" | "AVERAGE" | "BIG";
+  energy_level?:
+    | "VERY_LOW_ENERGY"
+    | "LOW_ENERGY"
+    | "MODERATE_ENERGY"
+    | "HIGH_ENERGY"
+    | "VERY_HIGH_ENERGY";
+
+  independence?: "LOW" | "AVERAGE" | "HIGH";
+}
+
 export interface PetsRepository {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
   findByCEP(cityCep: string): Promise<Pet[]>;
+  fetchByAttributes(attribute: FilterProps, cityCep: string): Promise<Pet[]>;
 }
