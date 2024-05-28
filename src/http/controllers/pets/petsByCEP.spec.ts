c14d24457bc3cd5eb93b6951d02ba351b4a892b9
Icaro Ramos <polosnramos@hotmail.com>
2024-05-28 10:29:28 -0300

    test: Create e2e tests for fetch pet by CEP

---
+++ src/http/controllers/pets/petsByCEP.spec.ts
@@ -0,0 +1,69 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { CreateAndAuthenticateOrg } from "@/utils/test/create-authenticate-org";
+import { prisma } from "@/lib/prisma";
+
+describe("Fetch pet by CEP (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should pick up all pets from a CEP", async () => {
+    const { token, org } = await CreateAndAuthenticateOrg(app);
+
+    await prisma.pet.create({
+      data: {
+        orgId: org.id,
+        name: "Pet",
+        about: "Nice dog.",
+        age: "PUPPY",
+        size: "SMALL",
+        energy_level: "VERY_LOW_ENERGY",
+        independence: "LOW",
+        ambient: "WIDE",
+        photo: [
+          "https://example.com/images/pet1.jpg",
+          "https://example.com/images/pet2.jpg",
+        ],
+        requirement: [
+          "Espaço para correr",
+          "Brinquedos para morder",
+          "Companhia frequente",
+        ],
+      },
+    });
+    await prisma.pet.create({
+      data: {
+        orgId: org.id,
+        name: "Pet2",
+        about: "Nice dog.",
+        age: "PUPPY",
+        size: "SMALL",
+        energy_level: "VERY_LOW_ENERGY",
+        independence: "LOW",
+        ambient: "WIDE",
+        photo: [
+          "https://example.com/images/pet1.jpg",
+          "https://example.com/images/pet2.jpg",
+        ],
+        requirement: [
+          "Espaço para correr",
+          "Brinquedos para morder",
+          "Companhia frequente",
+        ],
+      },
+    });
+
+    const response = await request(app.server)
+      .get(`/pets/${1234}`)
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.pets).toHaveLength(2);
+  });
+});
